9170d51b950e911d95ad9689e23c55cc4fdb5620
Austin Guiney <austin.guiney@gmail.com>
2023-02-25 11:35:40 -0500

    Added square-shaped and cross-shaped movement patterns.

--- prototypes/movement_patterns_prototype.py
+++ prototypes/movement_patterns_prototype.py
@@ -24,9 +24,9 @@ def move_in_circle_pattern(velocity, radius, seconds):
         rear_left_wheel_speed, rear_left_wheel_speed)
         time.sleep(0.05)
 
-def move_in_cross_pattern(seconds):
+def move_in_x_pattern(seconds):
     """
-    Moves the Robomaster in a cross pattern.
+    Moves the Robomaster in an X-shaped pattern.
   
     Parameters:
     seconds (int): The duration of the movement pattern in seconds.
@@ -35,11 +35,46 @@ def move_in_cross_pattern(seconds):
     void
     """
     chassis_ctrl.set_trans_speed(1)
-    chassis_ctrl.move_with_time(90,math.sqrt(seconds))
-    chassis_ctrl.move_with_time(-45,seconds)
-    chassis_ctrl.move_with_time(90,math.sqrt(seconds))
-    chassis_ctrl.move_with_time(-135,seconds)
+    chassis_ctrl.move_with_time(90, math.sqrt(seconds))
+    chassis_ctrl.move_with_time(-45, seconds)
+    chassis_ctrl.move_with_time(90, math.sqrt(seconds))
+    chassis_ctrl.move_with_time(-135, seconds)
     chassis_ctrl.stop()
+
+def move_in_cross_pattern(distance):
+    """
+    Moves the Robomaster in an cross-shaped pattern.
+  
+    Parameters:
+    distance (int): The vertical and horizontal distance in to travel in m. Has a range of [0, 5] m.
+  
+    Returns:
+    void
+    """
+    chassis_ctrl.set_trans_speed(1.5)
+    chassis_ctrl.move_with_distance(0, distance)
+    chassis_ctrl.move_with_distance(180, distance / 2)
+    chassis_ctrl.move_with_distance(-90, distance / 2)
+    chassis_ctrl.move_with_distance(90, distance)
+
+def move_in_square_pattern(velocity, wait):
+    """
+    Moves the Robomaster in an square-shaped pattern.
+  
+    Parameters:
+    velocity (int): The velocity of the Robomaster's wheels in RPM.
+    wait (int): The waiting period in seconds before completing another side of the square.
+  
+    Returns:
+    void
+    """
+    chassis_ctrl.set_wheel_speed(velocity,-velocity,-velocity,velocity)
+    time.sleep(wait)
+    chassis_ctrl.set_wheel_speed(velocity,velocity,velocity,velocity)
+    time.sleep(wait)
+    chassis_ctrl.set_wheel_speed(-velocity,velocity,velocity,-velocity)
+    time.sleep(wait)
+    chassis_ctrl.set_wheel_speed(-velocity,-velocity,-velocity,-velocity)
 
 def start():
     """
@@ -48,4 +83,6 @@ def start():
     Moves the Robomaster in a movement pattern.
     """
     move_in_circle(130, 1, 15)
-    #move_in_cross_pattern(2)
+    #move_in_x_pattern(2)
+    #move_in_cross_pattern(1)
+    #move_in_square_pattern(100, 2)
